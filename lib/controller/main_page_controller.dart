914ba49fc78393eca6726db1b3793d679557b1da
Mohammadb <mohammad.mb.bashirinia@gmail.com>
2024-07-29 06:13:58 +0330

    create main controller

--- lib/controller/main_page_controller.dart
+++ lib/controller/main_page_controller.dart
@@ -1,3 +1,11 @@
 import 'package:get/get.dart';
+import 'package:trip/models/form_model.dart';
+import 'package:trip/utils/repository.dart';
 
-class MainPageController extends GetxController {}
+class MainPageController extends GetxController {
+  List<FormModel>? listFormModel;
+
+  Future<void> getListFormModels() async {
+    listFormModel = await Repository.readExcelFile();
+  }
+}
